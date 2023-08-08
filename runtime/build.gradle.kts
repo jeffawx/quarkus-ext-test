0fe46bcedb0f2264f8bcee14d707b02c5788adb4
Jeff Fang <jeff.fang@airwallex.com>
2023-08-08 11:15:55 +0800

    add application

--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -1,11 +1,14 @@
 plugins {
     id("io.quarkus.extension")
     kotlin("jvm")
+    kotlin("kapt")
 }
 
 dependencies {
     implementation(platform("io.quarkus:quarkus-bom:3.2.2.Final"))
     implementation("io.quarkus:quarkus-undertow")
+    kapt("io.quarkus:quarkus-extension-processor:3.2.2.Final")
+    kaptTest("io.quarkus:quarkus-extension-processor:3.2.2.Final")
 }
 
 quarkusExtension {
